1632efa3a7d40ea0b9a9b6090919af7431cfc72b
niccybee <niccybee@users.noreply.github.com>
2022-09-06 18:48:55 +1000

    added: 'video' 'info box' 'firstbox' 'multichoice'

---
+++ src/components/CardMultiChoice.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  data: {},
+});
+</script>
+<template>
+  <div class="">
+    <h1>{{ props.data.title }}</h1>
+    <p>{{ data.body }}</p>
+    <div class="bg-gray-100 rounded-md p-2">
+      <p>{{ data.content.question }}</p>
+      <div
+        class="bg-white mb-1 p-2 hover:bg-blue-100 hover:outline hover:outline-blue-500 rounded-md p-2 cursor-pointer"
+        v-for="item in data.content.multi" @click="emit"
+      >
+        {{ item.text }}
+      </div>
+    </div>
+  </div>
+</template>
